8ac1d11b2632841fa9bcee420d5f15c0ef4be839
Edward Tirado Jr <edward.tirado@outlook.com>
2015-03-14 15:15:35 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # chatbot
 A script to send a message as someone else in Slack
+
+
+Setup
+-------------
+For this to work you'll need a webhook for your chat group.  You can get that here:
+https://my.slack.com/services/new/incoming-webhook/
+
+Then you need to tell your script to use that hook. You have a couple of options for that.
+
+1) Add an environment variable named 'SLACK_HOOK' to your OS that contains your web hook. No code change necessary.
+
+2) Change the line in chatbot.py that says: 
+url = os.environ['SLACK_HOOK'] 
+
+to 
+
+url = 'http://yourwebhook'
+
+
+That should be enough to get you up and running with the default characters.  Just follow the directions to send a message.
+
+
+Adding/changing characters
+--------------------------
+The script pulls a list of characters from characters.json.  The list contains a set of pairs than contain the username and image for the message sender.  Add or remove them as needed.
---
+++ chatbot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import os
+import urllib
+import json
+
+url = os.environ['SLACK_HOOK']
+
+characterJSON = open('characters.json').read()
+output_json = json.loads(characterJSON)
+totalCharacters = len(output_json["characters"])
+
+for index, character in enumerate(output_json["characters"]):
+        print(str(index + 1) + ') ' + character["username"])
+
+        if index == totalCharacters - 1:
+                print(str(index + 2) + ') Other')
+
+
+character = raw_input("Choose a character => ")
+channel = raw_input("Enter channel => ")
+
+
+#Enter character if 'Other' is chosen
+if int(character) == totalCharacters + 1:
+    username = raw_input("Enter username => ")
+    icon_url = raw_input("Enter image url => ")
+
+else:
+    username = output_json["characters"][int(character) - 1]['username']
+    icon_url = output_json["characters"][int(character) - 1]['icon_url']
+
+
+message = raw_input("Enter message => ")
+params = {
+            "username":username,
+            "icon_url":icon_url,
+            "text": message,
+            "channel":channel,
+            "link_names":1
+         }
+
+data = json.dumps(params)
+req = urllib.urlopen(url, data)
+print(req.read()) #Display send status
